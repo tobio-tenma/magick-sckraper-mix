730a97cf192057023ae0d25b09760f4fcd9821a3
tobio-tenma <tobio.tenma@dead-dreamer.net>
2023-10-14 05:10:59 -0600

    created SH version of script for executing on-device.

---
+++ magick-skraper-mix.sh
@@ -0,0 +1,18 @@
+# background prep
+magick .\background.png -resize 333x480^ -gravity center -extent 333x480 background_resized.png
+
+# gradient prep
+magick gradient.png -resize 333x240! gradient_resized.png
+magick gradient_resized.png -channel A -evaluate multiply 0.75 gradient_resized.png
+
+# logo prep
+magick logo.png -background-none -resize 316.35x120^ -gravity center -extent 316.35x120 logo_resized.png
+magick logo_resized.png -background none -splice 0x10 logo_padded.png
+
+# create composite of gradient and background
+magick background_resized.png gradient_resized.png -gravity North -composite gradient_background.png
+
+# create composite of logo and gradient-background
+magick gradient_background.png logo_padded.png -background none -gravity North -composite final_output.png
+
+echo "Sucessfully completed composition."
